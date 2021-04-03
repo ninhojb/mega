01c370a146980e75795747b505f4f74f0f05ed2b
ninhojb <ninhojb@bol.com.br>
2021-04-03 12:37:00 -0300

    incluir resultado manualmente

--- docker/scripts/airflow-entrypoint.sh
+++ docker/scripts/airflow-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 airflow db upgrade
 airflow webserver
+sudo docker exec -it docker_webserver_1 bash
 airflow users create -r Admin -u admin -e ninhojb@gmail.com -f admin -l user -p Mastermind@10
 airflow users create -r Admin -u ninhojb -e ninhojb@gmail.com -f admin -l user -p Mastermind@10
